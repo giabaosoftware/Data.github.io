da15c56b59a46ab490c1c670e14092adb53cc983
giabaosoftware <baobao.software@gmail.com>
2021-10-21 09:57:32 +0700

    add jQuery

--- js/index.js
+++ js/index.js
@@ -1,5 +1,75 @@
 //jshint esversion: 6
-class Stack{
+
+
+
+// let elName = document.getElementById("nameInput");
+// let elData = document.getElementById("dataInput");
+// let elTable = document.getElementById("table-body");
+// let elSearch = document.getElementById("searchInput");
+// let data = new Array();
+
+
+// //display data
+// function displayData(item){
+//     const newRow = document.createElement('tr');
+//     newRow.innerHTML = `
+//                         <td>${item.name}</td>
+//                         <td class="flex justify-between align-center">${item.data} <i class="bi bi-x-circle-fill" onclick="removeItem('${item.name}','${item.data}')"></i>
+//                         </td>
+//                         `;
+//     elTable.appendChild(newRow);
+//     console.log(item);
+// }
+
+
+// //get data
+// function getData(){
+//     elTable.innerHTML = "";
+//     if(elName.value){
+//         data.push({name : elName.value, data : elData.value});
+//         data.stack.forEach(data => displayData(data));
+//         console.log(data.stack);
+//     }else{
+//         data.stack.forEach(data => displayData(data));
+//     }
+// }
+
+
+// //search data
+// function search(){
+//     elTable.innerHTML = "";
+//     data.stack.forEach(data => {
+//         if(elSearch.value){
+//             if(elSearch.value === data.name){
+//                 displayData(data);
+//             }
+//         }else{
+//             return false;
+//         }
+//     });
+//     if(!elSearch.value){
+//         data.stack.forEach(data => displayData(data));
+//     }
+// }
+
+
+// //remove data
+// function removeItem(name, content){
+//     for(let i = 0; i < data.stack.length; i++){
+//         if(data.stack[i].name === name){
+//             if(data.stack[i].data === content){
+//                 data.remove(data.stack[i]);
+//                 break;
+//             }
+//         }
+//     }
+//     elTable.innerHTML = "";
+//     data.stack.forEach(data => displayData(data));
+//     console.log(data);
+// }
+
+
+class Array{
     constructor(){
         this.stack = [];
     }
@@ -34,67 +104,69 @@ class Stack{
     }
 }    
 
-let elName = document.getElementById("nameInput");
-let elData = document.getElementById("dataInput");
-let elTable = document.getElementById("table-body");
-let elSearch = document.getElementById("searchInput");
-let data = new Stack();
-
-
-//display data
-function displayData(item){
-    const newRow = document.createElement('tr');
-    newRow.innerHTML = `<tr>
-                        <td>${item.name}</td>
-                        <td class="flex justify-between align-center">${item.data} <i class="bi bi-x-circle-fill" onclick="removeItem('${item.name}','${item.data}')"></i>
-                        </td>
-                        </tr>`;
-    elTable.appendChild(newRow);
-    console.log(item);
-}
+let data = new Array;
 
 //get data
-function getData(){
-    elTable.innerHTML = "";
-    if(elName.value){
-        data.push({name : elName.value, data : elData.value});
-        data.stack.forEach(data => displayData(data));
-        console.log(data.stack);
-    }else{
-        data.stack.forEach(data => displayData(data));
-    }
+$(document).ready(function(){
+    $("#data").click(function(){
+        if($("#nameInput").val()){ 
+            data.push({name : $("#nameInput").val(), data : $("#dataInput").val()});
+        }
+        displayData(data.stack);
+    });
+});
+
+//display data
+function displayData(array){
+        let html;
+        let count = 0;
+        array.forEach(item => {
+            html += `<tr>
+                     <td class="itemId-${count}">${count}</td>
+                     <td>${item.name}</td>
+                     <td class="flex justify-between align-center">${item.data}<i class="bi bi-x-circle-fill"></td>
+                     </tr>`;
+            $("#table-body").html(html);
+            count++;
+        });
 }
 
 //search data
-function search(){
-    elTable.innerHTML = "";
-    data.stack.forEach(data => {
-        if(elSearch.value){
-            if(elSearch.value === data.name){
-                displayData(data);
-            }
-        }else{
-            return false;
+$(document).ready(function(){
+    $("#search").click(function(){
+        let idSearch = $('#searchInput').val();
+        let id = $(`.itemId-${idSearch}`).text();
+        if(id){
+            let html = `<tr>
+                    <td>${id}</td>
+                    <td>${data.stack[id].name}</td>
+                    <td class="flex justify-between align-center">${data.stack[id].data}<i class="bi bi-x-circle-fill"></td>
+                    </tr>`;
+
+            $("#table-body").html(html);
+        } else {
+            displayData(data.stack);
         }
     });
-    if(!elSearch.value){
-        data.stack.forEach(data => displayData(data));
-    }
-}
+});
 
-//remove data
-function removeItem(name, content){
-    for(let i = 0; i < data.stack.length; i++){
-        if(data.stack[i].name === name){
-            if(data.stack[i].data === content){
-                data.remove(data.stack[i]);
-                break;
-            }
-        }
-    }
-    elTable.innerHTML = "";
-    data.stack.forEach(data => displayData(data));
-    console.log(data);
-}
+$(document).ready(function(){
+    $(".bi-x-circle-fill").click(function(){
+        console.log()
+    });
+});
 
-
+// //remove data
+// function removeItem(name, content){
+//     for(let i = 0; i < data.stack.length; i++){
+//         if(data.stack[i].name === name){
+//             if(data.stack[i].data === content){
+//                 data.remove(data.stack[i]);
+//                 break;
+//             }
+//         }
+//     }
+//     elTable.innerHTML = "";
+//     data.stack.forEach(data => displayData(data));
+//     console.log(data);
+// }
